2fd25b7f61a36f0f11f079f140b9e164114a4acc
maechel11 <jhoshemaechel01@gmail.com>
2024-12-22 22:15:02 +0700

    buat folder models dan file admin.ts, barang.ts

---
+++ app/models/admin.ts
@@ -0,0 +1,7 @@
+"use server";
+
+
+import { PrismaClient } from "@prisma/client";
+
+//buat variabel "prisma"
+const prisma = new PrismaClient ();
---
+++ app/models/barang.ts
@@ -0,0 +1,6 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+
+//buat variabel "prisma"
+const prisma = new PrismaClient ();
